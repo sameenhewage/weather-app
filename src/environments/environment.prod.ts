06c0457e0078a5906c8ede2605b9d04e42af9a6c
Sameen Hewage <sameen.android@gmail.com>
2022-12-07 20:58:27 +0530

    added api keys and base urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  X_RapidAPI_Key: '4e4780f65dmshb03ab222854b649p17007ajsnab0f8cbde8bc',
+  X_RapidAPI_Host_Weather: 'yahoo-weather5.p.rapidapi.com',
+  X_RapidAPI_Host_Cities: 'spott.p.rapidapi.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  X_RapidAPI_Key: '4e4780f65dmshb03ab222854b649p17007ajsnab0f8cbde8bc',
+  X_RapidAPI_Host_Weather: 'yahoo-weather5.p.rapidapi.com',
+  X_RapidAPI_Host_Cities: 'spott.p.rapidapi.com',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
