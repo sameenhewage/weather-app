ba14745f4bf53cb6de2420e1f33336ddd48da3af
Sameen Hewage <sameen.android@gmail.com>
2022-12-08 07:45:18 +0530

    added mock data for to help unit testing

---
+++ src/app/weather/mockdata/mockdata.ts
@@ -0,0 +1,418 @@
+export const weatherData =
+{
+    "location": {
+        "city": "New York",
+        "region": " NY",
+        "woeid": 2459115,
+        "country": "United States",
+        "lat": 40.71455,
+        "long": -74.007118,
+        "timezone_id": "America/New_York"
+    },
+    "current_observation": {
+        "wind": {
+            "chill": 10,
+            "direction": 0,
+            "speed": 9.6
+        },
+        "atmosphere": {
+            "humidity": 68,
+            "visibility": 16.01,
+            "pressure": 1021.3,
+            "rising": 0
+        },
+        "astronomy": {
+            "sunrise": "7:8 am",
+            "sunset": "4:29 pm"
+        },
+        "condition": {
+            "code": 33,
+            "text": "Mostly Clear",
+            "temperature": 11
+        },
+        "pubDate": 1670472600
+    },
+    "forecasts": [
+        {
+            "day": "Thu",
+            "date": 1670468400,
+            "low": 1,
+            "high": 12,
+            "text": "Mostly Cloudy",
+            "code": 28
+        },
+        {
+            "day": "Fri",
+            "date": 1670554800,
+            "low": 1,
+            "high": 8,
+            "text": "Partly Cloudy",
+            "code": 30
+        },
+        {
+            "day": "Sat",
+            "date": 1670641200,
+            "low": 3,
+            "high": 7,
+            "text": "Cloudy",
+            "code": 26
+        },
+        {
+            "day": "Sun",
+            "date": 1670727600,
+            "low": 3,
+            "high": 7,
+            "text": "Cloudy",
+            "code": 26
+        },
+        {
+            "day": "Mon",
+            "date": 1670814000,
+            "low": 0,
+            "high": 7,
+            "text": "Rain And Snow",
+            "code": 5
+        },
+        {
+            "day": "Tue",
+            "date": 1670900400,
+            "low": 1,
+            "high": 7,
+            "text": "Partly Cloudy",
+            "code": 30
+        },
+        {
+            "day": "Wed",
+            "date": 1670986800,
+            "low": 5,
+            "high": 8,
+            "text": "Showers",
+            "code": 11
+        },
+        {
+            "day": "Thu",
+            "date": 1671073200,
+            "low": 3,
+            "high": 10,
+            "text": "Rain",
+            "code": 12
+        },
+        {
+            "day": "Fri",
+            "date": 1671159600,
+            "low": 3,
+            "high": 7,
+            "text": "Showers",
+            "code": 11
+        },
+        {
+            "day": "Sat",
+            "date": 1671246000,
+            "low": 2,
+            "high": 5,
+            "text": "Showers",
+            "code": 11
+        },
+        {
+            "day": "Sun",
+            "date": 1671332400,
+            "low": 1,
+            "high": 5,
+            "text": "Mostly Sunny",
+            "code": 34
+        }
+    ]
+}
+
+export const cities = [
+    {
+        "id": "1248991",
+        "geonameId": 1248991,
+        "type": "CITY",
+        "name": "Colombo",
+        "population": 648034,
+        "elevation": 7,
+        "timezoneId": "Asia/Colombo",
+        "country": {
+            "id": "LK",
+            "geonameId": 1227603,
+            "name": "Sri Lanka"
+        },
+        "adminDivision1": {
+            "id": "LK.36",
+            "geonameId": 1223421,
+            "name": "Western Province"
+        },
+        "adminDivision2": {
+            "id": "LK.36.11",
+            "geonameId": 1248990,
+            "name": "Colombo District"
+        },
+        "score": 10.811598,
+        "coordinates": {
+            "latitude": 6.93548,
+            "longitude": 79.8487
+        }
+    },
+    {
+        "id": "3465927",
+        "geonameId": 3465927,
+        "type": "CITY",
+        "name": "Colombo",
+        "population": 217000,
+        "elevation": 1007,
+        "timezoneId": "America/Sao_Paulo",
+        "country": {
+            "id": "BR",
+            "geonameId": 3469034,
+            "name": "Brazil"
+        },
+        "adminDivision1": {
+            "id": "BR.18",
+            "geonameId": 3455077,
+            "name": "Paraná"
+        },
+        "adminDivision2": {
+            "id": "BR.18.4105805",
+            "geonameId": 6322737,
+            "name": "Colombo"
+        },
+        "score": 10.336462,
+        "coordinates": {
+            "latitude": -25.2917,
+            "longitude": -49.2242
+        }
+    },
+    {
+        "id": "1234569",
+        "geonameId": 1234569,
+        "type": "CITY",
+        "name": "Dehiwala-Mount Lavinia",
+        "population": 219827,
+        "elevation": 16,
+        "timezoneId": "Asia/Colombo",
+        "country": {
+            "id": "LK",
+            "geonameId": 1227603,
+            "name": "Sri Lanka"
+        },
+        "adminDivision1": {
+            "id": "LK.36",
+            "geonameId": 1223421,
+            "name": "Western Province"
+        },
+        "adminDivision2": {
+            "id": "LK.36.11",
+            "geonameId": 1248990,
+            "name": "Colombo District"
+        },
+        "score": 8.342083,
+        "coordinates": {
+            "latitude": 6.84019,
+            "longitude": 79.8712
+        }
+    },
+    {
+        "id": "1234633",
+        "geonameId": 1234633,
+        "type": "CITY",
+        "name": "Moratuwa",
+        "population": 168280,
+        "elevation": 10,
+        "timezoneId": "Asia/Colombo",
+        "country": {
+            "id": "LK",
+            "geonameId": 1227603,
+            "name": "Sri Lanka"
+        },
+        "adminDivision1": {
+            "id": "LK.36",
+            "geonameId": 1223421,
+            "name": "Western Province"
+        },
+        "adminDivision2": {
+            "id": "LK.36.11",
+            "geonameId": 1248990,
+            "name": "Colombo District"
+        },
+        "score": 8.267246,
+        "coordinates": {
+            "latitude": 6.773,
+            "longitude": 79.8816
+        }
+    },
+    {
+        "id": "1230089",
+        "geonameId": 1230089,
+        "type": "CITY",
+        "name": "Pita Kotte",
+        "population": 118179,
+        "elevation": 20,
+        "timezoneId": "Asia/Colombo",
+        "country": {
+            "id": "LK",
+            "geonameId": 1227603,
+            "name": "Sri Lanka"
+        },
+        "adminDivision1": {
+            "id": "LK.36",
+            "geonameId": 1223421,
+            "name": "Western Province"
+        },
+        "adminDivision2": {
+            "id": "LK.36.11",
+            "geonameId": 1248990,
+            "name": "Colombo District"
+        },
+        "score": 8.072544,
+        "coordinates": {
+            "latitude": 6.8905,
+            "longitude": 79.9015
+        }
+    },
+    {
+        "id": "1238992",
+        "geonameId": 1238992,
+        "type": "CITY",
+        "name": "Sri Jayewardenepura Kotte",
+        "population": 115826,
+        "elevation": 27,
+        "timezoneId": "Asia/Colombo",
+        "country": {
+            "id": "LK",
+            "geonameId": 1227603,
+            "name": "Sri Lanka"
+        },
+        "adminDivision1": {
+            "id": "LK.36",
+            "geonameId": 1223421,
+            "name": "Western Province"
+        },
+        "adminDivision2": {
+            "id": "LK.36.11",
+            "geonameId": 1248990,
+            "name": "Colombo District"
+        },
+        "score": 8.06381,
+        "coordinates": {
+            "latitude": 6.88297,
+            "longitude": 79.9071
+        }
+    },
+    {
+        "id": "1250164",
+        "geonameId": 1250164,
+        "type": "CITY",
+        "name": "Battaramulla South",
+        "population": 75633,
+        "elevation": 11,
+        "timezoneId": "Asia/Colombo",
+        "country": {
+            "id": "LK",
+            "geonameId": 1227603,
+            "name": "Sri Lanka"
+        },
+        "adminDivision1": {
+            "id": "LK.36",
+            "geonameId": 1223421,
+            "name": "Western Province"
+        },
+        "adminDivision2": {
+            "id": "LK.36.11",
+            "geonameId": 1248990,
+            "name": "Colombo District"
+        },
+        "score": 7.878717,
+        "coordinates": {
+            "latitude": 6.8964,
+            "longitude": 79.9181
+        }
+    },
+    {
+        "id": "1236854",
+        "geonameId": 1236854,
+        "type": "CITY",
+        "name": "Maharagama",
+        "population": 195355,
+        "elevation": 26,
+        "timezoneId": "Asia/Colombo",
+        "country": {
+            "id": "LK",
+            "geonameId": 1227603,
+            "name": "Sri Lanka"
+        },
+        "adminDivision1": {
+            "id": "LK.36",
+            "geonameId": 1223421,
+            "name": "Western Province"
+        },
+        "adminDivision2": {
+            "id": "LK.36.11",
+            "geonameId": 1248990,
+            "name": "Colombo District"
+        },
+        "score": 7.823324,
+        "coordinates": {
+            "latitude": 6.848,
+            "longitude": 79.9265
+        }
+    },
+    {
+        "id": "1239047",
+        "geonameId": 1239047,
+        "type": "CITY",
+        "name": "Kotikawatta",
+        "population": 64565,
+        "elevation": 16,
+        "timezoneId": "Asia/Colombo",
+        "country": {
+            "id": "LK",
+            "geonameId": 1227603,
+            "name": "Sri Lanka"
+        },
+        "adminDivision1": {
+            "id": "LK.36",
+            "geonameId": 1223421,
+            "name": "Western Province"
+        },
+        "adminDivision2": {
+            "id": "LK.36.11",
+            "geonameId": 1248990,
+            "name": "Colombo District"
+        },
+        "score": 7.8100038,
+        "coordinates": {
+            "latitude": 6.9269,
+            "longitude": 79.9095
+        }
+    },
+    {
+        "id": "1239593",
+        "geonameId": 1239593,
+        "type": "CITY",
+        "name": "Kolonnawa",
+        "population": 64887,
+        "elevation": 20,
+        "timezoneId": "Asia/Colombo",
+        "country": {
+            "id": "LK",
+            "geonameId": 1227603,
+            "name": "Sri Lanka"
+        },
+        "adminDivision1": {
+            "id": "LK.36",
+            "geonameId": 1223421,
+            "name": "Western Province"
+        },
+        "adminDivision2": {
+            "id": "LK.36.11",
+            "geonameId": 1248990,
+            "name": "Colombo District"
+        },
+        "score": 7.764004,
+        "coordinates": {
+            "latitude": 6.9329,
+            "longitude": 79.8848
+        }
+    }
+]
