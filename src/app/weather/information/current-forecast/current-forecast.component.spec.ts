f1bd0313aa1bf43835670e2bcc7163fce815c539
Sameen Hewage <sameen.android@gmail.com>
2022-12-08 07:45:40 +0530

    added unit testing for current forecast component

--- src/app/weather/information/current-forecast/current-forecast.component.spec.ts
+++ src/app/weather/information/current-forecast/current-forecast.component.spec.ts
@@ -1,25 +1,40 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { weatherData } from '../../mockdata/mockdata';
+import { WeatherService } from '../../services/weather.service';
 import { CurrentForecastComponent } from './current-forecast.component';
 
-describe('CurrentForecastComponent', () => {
+fdescribe('CurrentForecastComponent', () => {
   let component: CurrentForecastComponent;
   let fixture: ComponentFixture<CurrentForecastComponent>;
+  let weatherService: WeatherService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CurrentForecastComponent ]
+      declarations: [CurrentForecastComponent],
+      imports: [HttpClientTestingModule],
+      providers: []
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CurrentForecastComponent);
     component = fixture.componentInstance;
+    weatherService = TestBed.inject(WeatherService);
+    component.weatherData = weatherData;
+    //weatherService.setWeatherData(result);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show New York when click New York city', () => {
+    let cityName = fixture.debugElement.query(By.css(".city-name")).nativeElement.innerText;
+    expect(cityName).toEqual(weatherData.location.city)
+  });
+
 });
