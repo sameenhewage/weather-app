8630ca2372f7c89e648611b955b0f928d7efd482
Sameen Hewage <sameen.android@gmail.com>
2022-12-08 07:42:47 +0530

    added test case to weather component

--- src/app/weather/weather.component.spec.ts
+++ src/app/weather/weather.component.spec.ts
@@ -1,25 +1,51 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { weatherData } from './mockdata/mockdata';
+import { WeatherService } from './services/weather.service';
 import { WeatherComponent } from './weather.component';
 
 describe('WeatherComponent', () => {
   let component: WeatherComponent;
   let fixture: ComponentFixture<WeatherComponent>;
+  let weatherService: WeatherService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ WeatherComponent ]
+      declarations: [WeatherComponent],
+      imports: [HttpClientTestingModule],
+      providers: []
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(WeatherComponent);
     component = fixture.componentInstance;
+    weatherService = TestBed.inject(WeatherService);
+    weatherService.setWeatherData(weatherData);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should receive weather data on oninit ', () => {
+    expect(component.weatherData).toEqual(jasmine.objectContaining(weatherData));
+  });
+
+  it('should show cloudy-background-night class when code = 33', () => {
+    const className = component.getAppBackGround();
+    expect(className).toBe('cloudy-background-night');
+  });
+
+  it('should failed when code = 33', () => {
+    const className = component.getAppBackGround();
+    expect(className).toBe('cloudy-background-night');
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
 });
